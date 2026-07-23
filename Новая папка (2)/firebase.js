e98460173d8637fcc7db637e5a3eeebb83bcacf8
svflint1-cpu <svflint1@gmail.com>
2026-07-23 15:26:15 +0200

    Add files via upload

---
+++ Новая папка (2)/firebase.js
@@ -0,0 +1,17 @@
+// Firebase configuration
+// Вставь сюда свой firebaseConfig из Firebase Console
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDAdGHFkPEJpwd5Joml6y3M2MqdvauPwvI",
+  authDomain: "elektro-acecb.firebaseapp.com",
+  projectId: "elektro-acecb",
+  storageBucket: "elektro-acecb.firebasestorage.app",
+  messagingSenderId: "1055631627958",
+  appId: "1:1055631627958:web:71006491f452e982e93817"
+};
+
+// После заполнения можно подключить Firebase:
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
